e7fb72abc76717278580ed094b684aeafae85ff6
ProfessionalReally <nikita22408@gmail.com>
2024-04-12 20:41:03 +0700

    Написана тривиальная реализация метода setAlarm(). Тест на установку будильника на 08:00:00 пройден.

--- js/alarmClock.js
+++ js/alarmClock.js
@@ -1,5 +1,12 @@
 class alarmClock {
-  constructor() {}
+  constructor() {
+    this.alarmTime = "";
+  }
+
+  setAlarm(time) {
+    //todo реализовать метод setAlarm() позднее до конца
+    this.alarmTime = time;
+  }
 }
 
 module.exports = alarmClock;
