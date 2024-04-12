b3520e0ebc3f0908fdc72979c6ddd28d74772272
ProfessionalReally <nikita22408@gmail.com>
2024-04-12 20:31:09 +0700

    Добавлен тест для установки будильника на 08:00:00.

--- tests/alarmClock.test.js
+++ tests/alarmClock.test.js
@@ -1,7 +1,19 @@
-const alarmClock = require('../js/alarmClock');
+const alarmClock = require("../js/alarmClock");
 
 describe("AlarmClock class creation test", () => {
   it("should exist", () => {
     expect(alarmClock).toBeDefined();
+  });
+});
+
+describe("Alarm set test case", () => {
+  let alarm_Clock;
+  beforeEach(() => {
+    alarm_Clock = new alarmClock();
+  });
+  it("set alarm for 08:00:00", () => {
+    const time = "08:00:00";
+    alarm_Clock.setAlarm(time);
+    expect(alarm_Clock.alarmTime).toBe(time);
   });
 });
