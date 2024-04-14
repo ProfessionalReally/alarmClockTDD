af1b181f19efcea9e5955be530b4ea26bad88278
ProfessionalReally <nikita22408@gmail.com>
2024-04-14 18:12:40 +0700

    Модифицируем метод updateTime(), чтобы оба теста проходило. Тесты пройдены.

--- js/alarmClock.js
+++ js/alarmClock.js
@@ -1,11 +1,17 @@
 class alarmClock {
   constructor() {
-    this.alarmTime = '';
-    this.currentTime = '';
+    this.alarmTime = "";
+    this.currentTime = "";
   }
 
   updateTime(currentTime) {
-    this.currentTime = currentTime;
+    if (currentTime instanceof Date) {
+      const hours = currentTime.getHours().toString().padStart(2, "0");
+      const minutes = currentTime.getMinutes().toString().padStart(2, "0");
+      const seconds = currentTime.getSeconds().toString().padStart(2, "0");
+      const timeString = `${hours}:${minutes}:${seconds}`;
+      this.currentTime = timeString;
+    } else this.currentTime = currentTime;
   }
 
   /**
